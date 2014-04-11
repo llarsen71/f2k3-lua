7ca698c5a35e49032a479dcaca44b635cccb3a72
Lance Larsen <llarsen@islinc.com>
2014-04-11 08:46:37 -0600

    Added callback functions to Lua

--- test/FFI_Test.f90
+++ test/FFI_Test.f90
@@ -7,7 +7,32 @@ module FFI_Test
     integer i, j
   end type mydata
   
+  abstract interface
+    subroutine sendMyData_(info) bind(C)
+      import :: mydata
+      type(mydata) :: info
+    end subroutine
+  end interface
+  
+  procedure(sendMyData_), pointer :: sendMyData => NULL()
+  
   contains
+
+  subroutine registerLuaCallback(callback)
+! The following exports this function so that it can be called
+! using the LuaJIT FFI interface.
+!DEC$ ATTRIBUTES DLLEXPORT,ALIAS:"registerLuaCallback" :: registerLuaCallback
+    use iso_c_binding, only: c_funptr, c_f_procpointer
+    implicit none
+    type(c_funptr), value :: callback
+    type(mydata) :: info
+    
+    call c_f_procpointer(callback, sendMyData)
+    
+    info%i = 15
+    info%j = 25
+    call sendMyData(info)
+  end subroutine
   
   subroutine callme(info)
 ! The following exports this function so that it can be called
@@ -19,5 +44,5 @@ module FFI_Test
     
     write(*,*) info%i, info%j
   end subroutine
-
+  
 end module FFI_Test
--- test/example.lua
+++ test/example.lua
@@ -5,6 +5,8 @@ local ffi = require("ffi")
 ffi.cdef[[
 typedef struct { int i, j; } mydata;
 void callme(mydata *info);
+typedef void (__stdcall *GETMYDATA)(mydata *info);
+void registerLuaCallback(GETMYDATA fn);
 ]]
 -- Note that the FORTRAN callme function needs to have a DDLEXPORT call, and
 -- this needs to set the exported name to callme. It will then be available
@@ -17,5 +19,15 @@ local info = ffi.new("mydata")
 info.i = 10
 info.j = 20
 ffi.C.callme(info)
+i = i + 1
+print(i)
+
+function getMyData(data)
+  print(type(data))
+  print(data.i .. ":" .. data.j)
+end
+
+ffi.C.registerLuaCallback(getMyData)
+
 i = i + 1
 print(i)
